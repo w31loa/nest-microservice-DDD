6e2b1c54ad7c606dca4b12650405fa94bbbf372a
w31loa <alexslava.barannik@gmail.com>
2024-02-04 18:29:11 +0500

    ADD: global exceptoin filter

--- libs/shared/src/shared.module.ts
+++ libs/shared/src/shared.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
-import { SharedService } from './shared.service';
+import { AllExceptionsFilter } from './filters';
+import { APP_FILTER } from '@nestjs/core';
 
-@Module({
-  providers: [SharedService],
-  exports: [SharedService],
+@Module({ //подключение фильтра после чего подключаем его в апп модуль и он будет работать глобально по всему приложению
+  providers: [{
+    provide: APP_FILTER, //константа неста для подключения фильтра ошибок
+    useClass: AllExceptionsFilter
+  }],
+  exports: [],
 })
 export class SharedModule {}
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { ProvidersModule } from '@lib/providers/providers.module';
+import { SharedModule } from '@lib/shared';
 import { Module } from '@nestjs/common';
 
 @Module({
   imports: [
+    SharedModule, // просто добавляем в импорты модуль
     ProvidersModule
   ],
 })
