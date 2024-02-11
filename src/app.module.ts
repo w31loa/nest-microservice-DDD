688e2d4b040d38cdeadd311123a82ff1f3bc8d41
w31loa <alexslava.barannik@gmail.com>
2024-02-11 22:45:51 +0500

    ADD: controllers | post controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { ProvidersModule } from '@lib/providers/providers.module';
 import { SharedModule } from '@lib/shared';
 import { Module } from '@nestjs/common';
+import { ApiModule } from './api';
 
 @Module({
   imports: [
     SharedModule, // просто добавляем в импорты модуль
-    ProvidersModule
+    ProvidersModule, ApiModule
   ],
 })
 export class AppModule {}
