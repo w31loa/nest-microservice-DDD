0dc55c6c08820bf3fb7c4a356f69855470efc956
w31loa <alexslava.barannik@gmail.com>
2024-02-13 00:40:39 +0500

    ADD: currnet user interface

--- libs/auth/src/decorators/current-user.decorator.ts
+++ libs/auth/src/decorators/current-user.decorator.ts
@@ -1,6 +1,7 @@
 import { ExecutionContext, createParamDecorator } from "@nestjs/common";
+import { ICurrentUser } from "../interfaces";
 
-export const CurrentUser = createParamDecorator((data:unknown , ctx:ExecutionContext)=>{
+export const CurrentUser = createParamDecorator((data:unknown , ctx:ExecutionContext):ICurrentUser=>{
     const req = ctx.switchToHttp().getRequest()
     return req.user
 })
